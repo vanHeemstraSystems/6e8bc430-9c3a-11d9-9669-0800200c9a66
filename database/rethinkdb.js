eae79ae89d90205715c5c02efb71fc04a1baa529
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-30 18:33:03 +0100

    Modified

--- database/rethinkdb.js
+++ database/rethinkdb.js
@@ -12,6 +12,11 @@ function Rethinkdb() {
   self._authKey = "";
   self._db = "test";
   self._rethinkdbdash = {}; // will be set by server, before passing on to mapping
+  self._type = {};  // will be set by server, before passing on to mapping
+  self._schema = {};   // will be set by server, before passing on to mapping
+  self._utility = {};  // will be set by server, before passing on to mapping
+  self._error = {};  // will be set by server, before passing on to mapping
+  self._event = {};  // will be set by server, before passing on to mapping
   // Dummy public variables  
   self._default = undefined;
   self._validator = undefined;
@@ -40,6 +45,46 @@ Rethinkdb.prototype.rethinkdbdash = function() {
 
 Rethinkdb.prototype.setrethinkdbdash = function(fnOrValue) {
   self._rethinkdbdash = fnOrValue;
+}
+
+Rethinkdb.prototype.type = function() {
+  return self._type;
+}
+
+Rethinkdb.prototype.settype = function(fnOrValue) {
+  self._type = fnOrValue;
+}
+
+Rethinkdb.prototype.schema = function() {
+  return self._schema;
+}
+
+Rethinkdb.prototype.setschema = function(fnOrValue) {
+  self._schema = fnOrValue;
+}
+
+Rethinkdb.prototype.utility = function() {
+  return self._utility;
+}
+
+Rethinkdb.prototype.setutility = function(fnOrValue) {
+  self._utility = fnOrValue;
+}
+
+Rethinkdb.prototype.error = function() {
+  return self._error;
+}
+
+Rethinkdb.prototype.seterror = function(fnOrValue) {
+  self._error = fnOrValue;
+}
+
+Rethinkdb.prototype.event = function() {
+  return self._event;
+}
+
+Rethinkdb.prototype.setevent = function(fnOrValue) {
+  self._event = fnOrValue;
 }
 
 Rethinkdb.prototype.default = function(fnOrValue) {
